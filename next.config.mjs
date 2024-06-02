7b29161f94e7932322fcf43e81963830aba54e4d
qqsh1ne <qqsh1neee@gmail.com>
2024-06-02 17:05:48 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
         source: "/",
         destination: "/home",
         permanent: true,
+        env: {
+          NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+        },
       },
     ];
   },
